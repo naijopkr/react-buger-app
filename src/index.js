ae5d6ddc927c3236cc19d1c5bf0c9cfe5bf93be0
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-10-17 19:13:33 -0500

    Added redux dev tools

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App';
 import reducer from './store/reducer'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 const app = (
   <Provider store={store}>
