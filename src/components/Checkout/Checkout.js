da4313a6e45cfd332bef585d5d630a1d0c67d365
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-10-24 19:51:16 -0500

    redirecting from checkout when there is no ingredients

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import CheckoutSummary from './CheckoutSummary/CheckoutSummary'
@@ -16,19 +16,23 @@ class Checkout extends Component {
   }
 
   render = () => {
-    return (
-      <div>
-        <CheckoutSummary 
-         ingredients={this.props.ingredients}
-         onCheckoutContinued={this.onCheckoutContinued}
-         onCheckoutCancelled={this.onCheckoutCancelled} 
-        />
-        <Route 
-         path={this.props.match.path + '/contact-data'} 
-         component={ContactData}
-        />
-      </div>
-    )
+    let summary = <Redirect to='/' />
+    if (this.props.ingredients) {
+      summary = (
+        <div>
+          <CheckoutSummary 
+          ingredients={this.props.ingredients}
+          onCheckoutContinued={this.onCheckoutContinued}
+          onCheckoutCancelled={this.onCheckoutCancelled} 
+          />
+          <Route 
+          path={this.props.match.path + '/contact-data'} 
+          component={ContactData}
+          />
+        </div>
+      )
+    }
+    return summary
   }
 }
 
